a6bfa410a40071ce2de149a7391b6b6d94c0c1bf
stanislav.spatari <stanislav.spatari@saltedge.com>
2019-06-28 01:03:58 +0300

    Set a global prefix for all routes

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ const port = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
