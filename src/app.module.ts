5e265c558caddf1ee3e571d1230bfd1ade8fb505
stanislav.spatari <stanislav.spatari@saltedge.com>
2019-06-30 02:56:16 +0300

    Remove unecesarry app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), AuthorsModule, BooksModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
