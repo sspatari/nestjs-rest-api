8c7f409a43be79940043f1b8a0229099904ad93c
stanislav.spatari <stanislav.spatari@saltedge.com>
2019-07-08 00:06:11 +0300

    Create LoggingInterceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
 import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), AuthorsModule, BooksModule],
@@ -11,6 +12,10 @@ import { HttpErrorFilter } from './shared/http-error.filter';
     {
       provide: APP_FILTER,
       useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
     },
   ],
 })
